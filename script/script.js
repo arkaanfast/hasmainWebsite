e4cdba4a4e74583e658c02adeebb9a3fadc5f7a2
arkaansayed <arkaan@notionpress.com>
2023-04-08 02:29:49 +0530

    Carousal removed

--- script/script.js
+++ script/script.js
@@ -10,6 +10,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
             $('nav.navbar#my-nav').css("background-color", "white");
             $('nav.navbar#my-nav li a').css("color", "black");
             $('.navbar-nav .active a').css("color", "#f1ab3d");
+            $('#nav-dropdown li a').css("color", "black");
+            $('nav.navbar.navbar-toggler').css("color", "white");
         } else {
             $('nav.navbar#my-nav').css("background-color", "transparent");
             $('nav.navbar#my-nav li a').css("color", "white");
